e0e52d0146fc37a8b925daf7758883f8c98ffe69
danielaibero <dcarva20@estudiante.ibero.edu.co>
2026-05-03 21:52:07 -0500

    Fix base de datos para produccion

--- nexopyme/nexopyme/settings.py
+++ nexopyme/nexopyme/settings.py
@@ -79,14 +79,12 @@ WSGI_APPLICATION = 'nexopyme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'nexopyme',
-        'USER': 'root',
-        'PASSWORD': 'Luciana1',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
